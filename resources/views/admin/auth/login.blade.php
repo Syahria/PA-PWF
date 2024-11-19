e1cb0920c78cf25cb87ebee30b85f2e48102f215
nurullidn <127465143+nurullidn@users.noreply.github.com>
2024-11-19 16:46:22 +0800

    Update login.blade.php

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login</title>
+    <title>Desa Pampang</title>
 
     <!-- Link CSS Bootstrap dari CDN -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
@@ -63,7 +63,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Wisata Desa Pampang</a>
+        <a class="navbar-brand" href="#" style="padding-left: 15px;"><img src="assets/logo.jpg" alt="Logo" style="height: 30px; width: 30px; border-radius: 50%; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); margin-right: 8px;"> Wisata Desa Pampang</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -77,9 +77,6 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/wisata') }}">Wisata</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/gallery') }}">Gallery</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/fasilitas') }}">Fasilitas</a>
