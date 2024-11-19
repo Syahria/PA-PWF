254db0e913cd0dc184f029e29ef0bc771e97795f
nurullidn <127465143+nurullidn@users.noreply.github.com>
2024-11-19 16:50:58 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda</title>
+    <title>Desa Pampang</title>
     <link rel="stylesheet" href="{{ asset('assets/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -50,7 +50,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Wisata Desa Pampang</a>
+    <a class="navbar-brand" href="#" style="padding-left: 15px;"><img src="assets/logo.jpg" alt="Logo" style="height: 30px; width: 30px; border-radius: 50%; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); margin-right: 8px;"> Wisata Desa Pampang </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -64,9 +64,6 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/wisata') }}">Wisata</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/gallery') }}">Gallery</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/fasilitas') }}">Fasilitas</a>
