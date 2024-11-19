43f1083de0795b754079a1871de8d3283fb377ca
nurullidn <127465143+nurullidn@users.noreply.github.com>
2024-11-19 16:50:02 +0800

    Update Fasilitas.blade.php

--- resources/views/Fasilitas.blade.php
+++ resources/views/Fasilitas.blade.php
@@ -9,29 +9,29 @@
     
     <div class="row">
         <div class="col-md-4 mb-4">
-            <div class="card border-0 shadow">
+            <div class="card border-0 shadow" style="background-color: #FC8F54;">
                 <img src="{{ asset('assets/parkir1.jpg') }}" class="card-img-top" alt="Penginapan" />
                 <div class="card-body text-center">
-                    <h5 class="card-title">Area Parkir</h5>
-                    <p class="card-text">Area parkir yang aman dan nyaman untuk kendaraan Anda selama berkunjung.</p>
+                    <h5 class="card-title" style="color: white;">Area Parkir</h5>
+                    <p class="card-text" style="color: white;">Area parkir yang aman dan nyaman untuk kendaraan Anda selama berkunjung.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-4">
-            <div class="card border-0 shadow">
-                <img src="{{ asset('assets/desa.jpg') }}" class="card-img-top" alt="Pusat Informasi" />
+            <div class="card border-0 shadow" style="background-color: #FC8F54;">
+                <img src="{{ asset('assets/toilett.jpg') }}" class="card-img-top" alt="Pusat Informasi" />
                 <div class="card-body text-center">
-                    <h5 class="card-title">Toilet Umum</h5>
-                    <p class="card-text">Tersedia toilet umum bersih dan terawat untuk kebutuhan pengunjung desa.</p>
+                    <h5 class="card-title" style="color: white;">Toilet Umum</h5>
+                    <p class="card-text" style="color: white;">Tersedia toilet umum bersih dan terawat untuk kebutuhan pengunjung desa.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-4">
-            <div class="card border-0 shadow">
+            <div class="card border-0 shadow" style="background-color: #FC8F54;">
                 <img src="{{ asset('assets/shop.jpg') }}" class="card-img-top" alt="Tempat Parkir" />
                 <div class="card-body text-center">
-                    <h5 class="card-title">Warung Makan dan Kios Souvenir</h5>
-                    <p class="card-text">Terdapat warung makan dengan hidangan lokal dan kios souvenir khas Dayak.</p>
+                    <h5 class="card-title" style="color: white;">Warung Makan dan Kios Souvenir</h5>
+                    <p class="card-text" style="color: white;">Terdapat warung makan dengan hidangan lokal dan kios souvenir khas Dayak.</p>
                 </div>
             </div>
         </div>
@@ -39,29 +39,29 @@
 
     <div class="row">
         <div class="col-md-4 mb-4">
-            <div class="card border-0 shadow">
-                <img src="{{ asset('assets/panggung.jpg') }}" class="card-img-top" alt="Restoran" />
+            <div class="card border-0 shadow" style="background-color: #FC8F54;">
+                <img src="{{ asset('assets/tempatduduk.png') }}" class="card-img-top" alt="Restoran" />
                 <div class="card-body text-center">
-                    <h5 class="card-title">Tempat Duduk dan Gazebo</h5>
-                    <p class="card-text">Nikmati masakan lokal yang lezat di restoran kami yang nyaman.</p>
+                    <h5 class="card-title" style="color: white;">Tempat Duduk dan Gazebo</h5>
+                    <p class="card-text" style="color: white;">Tempat Duduk dan Gazebo untuk bersantai ataupun untuk menonton pertunjukan.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-4">
-            <div class="card border-0 shadow">
+            <div class="card border-0 shadow" style="background-color: #FC8F54;">
                 <img src="{{ asset('assets/panggung.jpg') }}" class="card-img-top" alt="Pertunjukan Seni" />
                 <div class="card-body text-center">
-                    <h5 class="card-title">Panggung Pertunjukan</h5>
-                    <p class="card-text">Saksikan pertunjukan seni dan budaya yang memukau selama kunjungan Anda.</p>
+                    <h5 class="card-title" style="color: white;">Panggung Pertunjukan</h5>
+                    <p class="card-text" style="color: white;">Saksikan pertunjukan seni dan budaya yang memukau selama kunjungan Anda.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-4">
-            <div class="card border-0 shadow">
-                <img src="{{ asset('assets/foto.jpeg') }}" class="card-img-top" alt="Jalan Setapak" />
+            <div class="card border-0 shadow" style="background-color: #FC8F54;">
+                <img src="{{ asset('assets/foto1.png') }}" class="card-img-top" alt="Jalan Setapak" />
                 <div class="card-body text-center">
-                    <h5 class="card-title">Spot Foto dan Selfie Area</h5>
-                    <p class="card-text">Jalan setapak yang ramah pejalan kaki untuk menjelajahi desa dengan nyaman.</p>
+                    <h5 class="card-title" style="color: white;">Spot Foto dan Selfie Area</h5>
+                    <p class="card-text" style="color: white;">Di sini, bisa berfoto bersama penduduk lokal dan mengenakan pakaian adatnya.</p>
                 </div>
             </div>
         </div>
